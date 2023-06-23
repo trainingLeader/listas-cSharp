71b1f44765eba0163246d708a660edda00eb49f7
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2023-06-23 07:31:58 -0500

    Clases y Regex email

--- Clases/Persona.cs
+++ Clases/Persona.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace listas_cSharp.Clases
@@ -18,12 +21,69 @@ namespace listas_cSharp.Clases
         public string Email { get => this.email ?? String.Empty; set => email = value; }
 
         public Persona(string idPerson, string name, int age, string email){
-            this.IdPerson = idPerson; 
+            this.IdPerson = GenerateShortId(Guid.NewGuid().ToString()); 
             this.Name =  name;
             this.Age = age; 
             this.Email = email;
         }
         public Persona(){
+            this.IdPerson = GenerateShortId(Guid.NewGuid().ToString());
+        }
+
+        public static string GenerateShortId(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(input));
+                string base64String = Convert.ToBase64String(hashBytes);
+                string shortId = base64String.Substring(0, 8); // Acorta a los primeros 8 caracteres
+                return shortId;
+            }
+        }
+
+        /*Validar email regex*/
+        public bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,45 @@
-﻿internal class Program
+﻿using listas_cSharp.Clases;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Persona alumno = new Persona();
+        bool isOkData = false;
+        bool isEmailOk = false;
+        List<Persona> personas = new List<Persona>();
+        while (isOkData == false){
+            Console.Clear();
+            try{
+                Console.WriteLine("Ingrese El nombre del Alumno");
+                alumno.Name = Console.ReadLine() ?? String.Empty;
+                if(alumno.Name == String.Empty){
+                    continue;
+                }
+                Console.WriteLine("Ingrese la edad del Alumno");
+                alumno.Age = Convert.ToInt32(Console.ReadLine());
+                do{
+                    Console.WriteLine("Ingrese El Email del Alumno");
+                    alumno.Email = Console.ReadLine() ?? String.Empty;
+                    isEmailOk = alumno.IsValidEmail(alumno.Email);
+                }while(isEmailOk == false);
+                personas.Add(alumno);
+                isOkData = true;
+            }catch(Exception){
+
+            }
+        }
+        Console.Clear();
+        Console.WriteLine("{0,-15} {1,20} {2,30} {3,40}","Codigo","Nombre del alumno","Edad","Correo electronico");
+        foreach (Persona person in personas){
+            try{
+                Console.WriteLine("{0,-15} {1,20} {2,30} {3,30}",person.IdPerson,person.Name,person.Age,person.Email);
+
+            }catch(Exception){
+                Console.WriteLine("Error verifique con chat-GPT");
+            }
+        }
+        Console.ReadKey();
+
     }
 }
